2b0f8039250a6d46bfdf5ef36a294f4a8b0ebffd
Unai Bercianos <unaibercianos98@gmail.com>
2025-05-04 18:23:46 +0200

    Error en la subida

--- src/main/java/test/TestEmpleadosDAO.java
+++ src/main/java/test/TestEmpleadosDAO.java
@@ -22,6 +22,7 @@ class TestEmpleadosDAO {
 
 	@Test
 	void testInsertar() throws SQLException {
+		//Orden de eliminar empleado con el siguiente email para evitar duplicados
 		dao.borrarPorEmail("test1@test.com");
 		Empleado e = new Empleado("test1","test1@test.com",3000,1);
 		dao.insertar(e);
@@ -34,6 +35,7 @@ class TestEmpleadosDAO {
 
 	@Test
 	void testActualizar() throws SQLException {
+		//Orden de eliminar empleado con el siguiente email para evitar duplicados
 		dao.borrarPorEmail("test2@test.com");
 		// Insertamos un empleado
 		Empleado e = new Empleado ("Test2","test2@test.com",3000,1);
@@ -47,7 +49,7 @@ class TestEmpleadosDAO {
 		ex.setEmail("actu@test.com");
 		ex.setSalario(3500);
 		ex.setDepartamento_id(1);
-		
+		//Actualizamos "base de datos"
 		int filas = dao.actualizarPorEmail("actu@test.com", ex);
 		assertEquals(1,filas);
 		
@@ -61,6 +63,7 @@ class TestEmpleadosDAO {
 
 	@Test
 	void testBorrar() throws SQLException {
+		//Orden de eliminar empleado con el siguiente email para evitar duplicados
 		dao.borrarPorEmail("test3@test.com");
 		//Insertamos nuevo empleado para luego borrarlo
 		Empleado e2 = new Empleado("Test3","test3@test.com",1234,2);
@@ -69,7 +72,7 @@ class TestEmpleadosDAO {
 		Empleado recu = dao.obtenerEmpleadoPorEmail("test3@test.com");
 		assertNotNull(recu);
 
-		// Borramos por email en lugar de por ID
+		// Borramos por email en lugar de por su email
 		int filas = dao.borrarPorEmail("test3@test.com");
 		assertEquals(1, filas);
 	
@@ -81,15 +84,17 @@ class TestEmpleadosDAO {
 
 	@Test
 	void testObtenerEmpleado() throws SQLException {
+		//Orden de eliminar empleado con el siguiente email para evitar duplicados
 		dao.borrarPorEmail("test4@test.com");
 		// Insertamos un empleado para luego recuperarlo
 	    Empleado e3 = new Empleado("test4", "test4@test.com", 2900, 3);
-	    dao.insertar(e3); // Insertamos el empleado y se le asigna un ID automáticamente
+	    dao.insertar(e3); 
 	    
+	    //Actualizamos "base de datos"	    
 	    int filas = dao.actualizarPorEmail("test4@test.com",e3);
 		assertEquals(1,filas);
 
-	    // Recuperamos el empleado por su ID
+	    // Recuperamos el empleado por su email
 	    Empleado ex3 = dao.obtenerEmpleadoPorEmail("test4@test.com");
 		assertNotNull(ex3);
 	    // Verificamos que los datos del empleado recuperado son correctos
